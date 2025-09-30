6aa4891838bd6b99ad512e015c623bcd60de5f0e
avaz <avazbek.rustambekov@ipotekabank.uz>
2025-09-30 09:42:29 +0500

    initial commit

---
+++ src/main/java/uz/lms/app/LmsAppApplication.java
@@ -0,0 +1,13 @@
+package uz.lms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/lms/app/LmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.lms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
